4db76c9e92191ae0258c01d9daed36fdc4ef4b47
TranLoc2k3 <tranloc120603@gmail.com>
2024-08-19 13:56:57 +0700

    Finish & add Image Part2/challenge 2

--- Javascript-fundamentals-part2.js
+++ Javascript-fundamentals-part2.js
@@ -19,5 +19,22 @@ function checkWinner(avgDolhins, avgKoalas) {
     }
 }
 
-checkWinner(calcAverage(44, 23, 71), calcAverage(65, 54, 49));
-checkWinner(calcAverage(85, 54, 41), calcAverage(23, 34, 27));
+// checkWinner(calcAverage(44, 23, 71), calcAverage(65, 54, 49));
+// checkWinner(calcAverage(85, 54, 41), calcAverage(23, 34, 27));
+
+// Coding Challenge #2
+function calcTip(bill) {
+    if (bill >= 50 && bill <= 300) {
+        return bill * 0.15;
+    }
+    return bill * 0.2;
+}
+
+
+let bills = [125, 555, 44]
+let tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+let toltal = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+
+console.log("Bills: ", bills);
+console.log("Tips: ", tips);
+console.log("Total: ", toltal);
