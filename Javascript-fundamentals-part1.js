67feab2a16c48729ce6cdf86313c7e0295be5893
TranLoc2k3 <tranloc120603@gmail.com>
2024-08-19 13:20:34 +0700

    Finish & add Image Part1/challenge 1

---
+++ Javascript-fundamentals-part1.js
@@ -0,0 +1,18 @@
+// Ho va ten: Tran Loc
+// MSSV: 21100321
+// Lab01- Javascript-fundamentals-part1
+
+
+// Coding Challenge #1
+let MarkMass = 78;
+let MarkHeight = 1.69;
+
+let JohnMass = 92;
+let JohnHeight = 1.95;
+
+let MarkBMI = MarkMass / MarkHeight ** 2;
+let JohnBMI = JohnMass / JohnHeight ** 2;
+
+let MarkHigherBMI = MarkBMI > JohnBMI;
+
+console.log("Mark is higher than John: " + MarkHigherBMI);
--- index.js
+++
@@ -1,4 +0,0 @@
-// Ho va ten: Tran Loc
-// MSSV: 21100321
-// Lab01
-
